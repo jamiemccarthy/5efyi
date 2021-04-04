ccb41f4dbd94a759ccd57c635990484da0347688
Jamie McCarthy <jamie@mccarthy.vg>
2021-04-03 19:12:00 -0500

    Refactor into SRD5Section base class

--- bin/emit-srd.rb
+++ bin/emit-srd.rb
@@ -39,67 +39,105 @@ class PDF::Reader::ColumnarReceiver < PDF::Reader::PageTextReceiver
   end
 end
 
-class TextRunWriter
-  def self.run_text_clean(run)
-    run_text = run.text.dup
-    run_text.gsub!(/[\s\u00a0]+/, " ") # if new_font_size < 10 ??
-    run_text.gsub!(/\s*-\u00ad\u2010\u2011?\s*/, "-")
-    run_text.strip!
-    run_text
-  end
-
-  def self.run_break?(run)
-    # Should we break to a new page starting with this run?
-    run.font_size >= 25
-  end
-
-  def self.break_into_sections(run_groups)
-    sections = [[]]
-    run_groups.each do |run_group|
-      next if run_group.count < 1
-      sections << [] if run_group[0].font_size == 25
-      sections[-1].concat(run_group)
+module SRD5Section
+  class Utility
+    def self.break_into_sections(run_groups)
+      sections = [[]]
+      run_groups.each do |run_group|
+        next if run_group.count < 1
+        sections << [] if run_group[0].font_size == 25
+        sections[-1].concat(run_group)
+      end
+      sections
     end
-    sections
   end
 
-  def self.subdir
-    "public"
-  end
+  class Base
+    attr_accessor :section_runs, :section_title, :section_filename
 
-  def self.mkdirs(dir, subdir)
-    Dir.mkdir(dir, 0755) unless Dir.exist?(dir)
-    abs_subdir = File.join(dir, subdir)
-    Dir.mkdir(abs_subdir, 0755) unless Dir.exist?(abs_subdir)
-  end
+    def self.create(section_runs)
+      section_title_runs = get_section_title_runs(section_runs)
+      # Skip any initial section that lacks a title ("If you note any errors...")
+      return nil unless section_title_runs.count > 0
 
-  def self.title_to_filename(title, dir)
-    filename = title.downcase.gsub(/[\s_:-]+/m, "-")
-    filename.gsub!(/^-+/, "")
-    filename.gsub!(/-+$/, "")
-    File.join(dir, subdir, filename)
-  end
+      self.get_subclass(section_title_runs).new(section_runs)
+    end
 
-  def self.write_section_file(filename, title, sections)
-    sections_by_size = sections.slice_when { |run_a, run_b| run_a.font_size != run_b.font_size }
-    File.open(filename, "w", 0644) do |io|
-      sections_by_size.each do |runs|
-        io.write(runs.map { |run| run_text_clean(run) }.join(" "), "\n")
+    def self.get_subclass(section_title_runs)
+      # TODO: fix this, it doesn't work for e.g. "Beyond 1st level" or "Using abilityScores"
+      subclass = section_title_runs.map { |run| run_text_clean(run).downcase.capitalize }.join("")
+      puts "subclass: #{subclass}"
+      Object.const_get("SRD5Section::#{subclass}") rescue nil || SRD5Section::Base
+    end
+
+    def self.get_section_title_runs(section_runs)
+      section_runs.select { |run| run_break?(run) }
+    end
+
+    def initialize(section_runs)
+      @section_runs = section_runs
+      @section_title = self.class.get_section_title(self.class.get_section_title_runs(section_runs))
+      @section_filename = self.class.get_section_filename(section_title)
+    end
+
+    def self.get_section_title(section_title_runs)
+      section_title_runs.map { |run| run_text_clean(run) }.join(" ")
+    end
+
+    def self.get_section_dir
+      Dir.pwd
+    end
+
+    def self.get_section_filename(section_title)
+      title_to_filename(section_title, get_section_dir)
+    end
+
+    def self.title_to_filename(title, dir)
+      filename = title.downcase.gsub(/[\s_:-]+/m, "-")
+      filename.gsub!(/^-+/, "")
+      filename.gsub!(/-+$/, "")
+      File.join(dir, subdir, filename)
+    end
+
+    def self.subdir
+      "public"
+    end
+
+    def self.run_text_clean(run)
+      run_text = run.text.dup
+      run_text.gsub!(/[\s\u00a0]+/, " ") # if new_font_size < 10 ??
+      run_text.gsub!(/\s*-\u00ad\u2010\u2011?\s*/, "-")
+      run_text.strip!
+      run_text
+    end
+
+    def section_runs_by_size
+      @section_runs.slice_when { |run_a, run_b| run_a.font_size != run_b.font_size }
+    end
+
+    def self.mkdirs
+      Dir.mkdir(get_section_dir, 0755) unless Dir.exist?(get_section_dir)
+      abs_subdir = File.join(get_section_dir, subdir)
+      Dir.mkdir(abs_subdir, 0755) unless Dir.exist?(abs_subdir)
+    end
+
+    def write_file
+      self.class.mkdirs
+      File.open(section_filename, "w", 0644) do |io|
+        section_runs_by_size.each do |runs|
+          io.write(runs.map { |run| self.class.run_text_clean(run) }.join(" "), "\n")
+        end
       end
     end
+
+    def self.run_break?(run)
+      # Should we break to a new page starting with this run?
+      run.font_size >= 25
+    end
   end
 
-  def self.write(sections, dir = Dir.pwd)
-    mkdirs(dir, subdir)
-    sections.each do |section_runs|
-      section_title_runs = section_runs.select { |run| run_break?(run) }
-      # Skip any initial section that lacks a title ("If you note any errors...")
-      next unless section_title_runs.count > 0
-
-      section_title = section_title_runs.map { |run| run_text_clean(run) }.join(" ")
-      section_filename = title_to_filename(section_title, dir)
-      write_section_file(section_filename, section_title, section_runs)
-    end
+  class Races < Base
+    # define a class that doesn't change anything just to make sure subclassing works
   end
 end
 
@@ -116,8 +154,11 @@ def emit_stuff(reader)
     pages[page_num].walk(receiver)
     run_groups.concat(receiver.two_column_run_groups)
   end
-  sections = TextRunWriter.break_into_sections(run_groups)
-  TextRunWriter.write(sections)
+  sections = SRD5Section::Utility.break_into_sections(run_groups)
+  sections.each do |section_runs|
+    obj = SRD5Section::Base.create(section_runs)
+    obj.write_file if obj
+  end
 end
 
 reader = get_reader
