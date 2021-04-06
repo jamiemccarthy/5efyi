7ffe1197dac728c59d304d04403add980b83f401
Jamie McCarthy <jamie@mccarthy.vg>
2021-04-06 18:46:09 -0500

    health_check

---
+++ config/initializers/health_check.rb
@@ -0,0 +1,5 @@
+HealthCheck.setup do |config|
+    config.standard_checks -= [ 'migrations' ]
+    config.origin_ip_whitelist = ''
+    # config.include_error_in_response_body = true # TODO once health_check gem makes this an option, post-3.0.0
+end
