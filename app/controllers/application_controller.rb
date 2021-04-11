31ec3440f0e5cc67873866fc1b2e612b2ffc61de
Jamie McCarthy <jamie@mccarthy.vg>
2021-04-11 18:21:29 -0500

    HomepageController, footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  mattr_accessor :srd_dir, :srd_page_files, instance_writer: false
+
+  @@srd_dir = File.join(Rails.public_path, "srd").freeze
+  @@srd_page_files = Dir.new(srd_dir).each_child.sort.freeze
+
+  def is_ogl?
+    false
+  end
 end
