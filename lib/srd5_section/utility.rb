70f1518f9fdba77dc27951063166b6bddcc38c74
Jamie McCarthy <jamie@mccarthy.vg>
2021-10-27 20:51:47 -0500

    monster section work

--- lib/srd5_section/utility.rb
+++ lib/srd5_section/utility.rb
@@ -6,6 +6,8 @@ module Srd5Section
       category = nil
       monsters_index = nil
       run_groups.each do |run_group|
+        # Strip out initial blank runs that have only whitespace and U+00A0, "No-Break Space"
+        run_group.shift while run_group.count > 0 && run_group[0].text.match?(/\A[\s\u00a0]*\z/)
         next if run_group.count < 1
 
         if current == "Monsters"
@@ -29,16 +31,36 @@ module Srd5Section
             # If it's in a category, maybe like append the category name to the first
             # run_group of this monster's section, and find a way to work it into
             # its text later
+            # Anyway, now we create a new section for this monster, into which this
+            # run_group and the following run_groups will be applied.
+            sections << []
             # If it's the first monster NOT in the previous category, now's a good time
             # to set category back to nil. I guess hard-code the list of "first monsters
             # not in the previous category" :/
-            # Anyway, now we create a new section for this monster, into which this
-            # run_group and the following run_groups will be applied
-            sections << []
+            if %W(
+              Ankheg
+              Doppelganger
+              Drider
+              Elf,.*Drow
+              Gargoyle
+              Ghost
+              Gibbering.*Mouther
+              Gorgon
+              Harpy
+              Magmin
+              Merfolk
+              Ogre
+              Orc
+              Specter
+              Sprite
+              Wight
+              ).any? { |monster| run_group[0].text.match? monster }
+              category = nil
+            end
           end
         elsif run_group[0].font_size == 25
           if current == "Monsters"
-            # time to move on to the appendixes, right? check what this run is
+            # time to move on to the appendixes, right? I think this is working, but need to check
             # byebug
           end
           current = run_group[0].text
